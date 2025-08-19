25a2f51d5e1873b13382317faf9f215aefa3d8dd
Gourab Ghosh <ggourab217@gmail.com>
2025-08-19 15:16:47 +0530

    assignment3.c

--- assignment3.c
+++ assignment3.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
-{int a=8, b=5;
-printf("%d",a-b);
+{int a,b,c;
+ printf("Enter Two Numbers:);
+ sacnf("%d,%d",&a,&b);
+ c=a-b;
+printf("Difference is:%d",c);
 return 0;
 }
