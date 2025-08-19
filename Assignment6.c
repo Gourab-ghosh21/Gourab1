8219a10197b72deb64f5c5ddf57f432fdf03e02a
Gourab Ghosh <ggourab217@gmail.com>
2025-08-19 15:55:26 +0530

    Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -7,7 +7,7 @@ printf("Enter the value of Rate of intrest:");
 scanf("%d",&r);
 printf("Enter the value of Time:");
 scanf("%d",&t);
-i=p*t*r%100;
+i=(p*t*r)/100;
 printf("the simple intrest is:%d",&i);
 return 0;
 }
