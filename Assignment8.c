c7f2232215f5372d33669cbd7c30a1ac44087bf2
Gourab Ghosh <ggourab217@gmail.com>
2026-01-13 22:37:51 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -1,4 +1,10 @@
-
+#include <stdio.h>
+int main() {
+    float celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("%.2f Celsius = %.2f Fahrenheit\n", celsius, fahrenheit);
     printf("Enter temperature in Fahrenheit: ");
     scanf("%f", &fahrenheit);
     celsius = (fahrenheit - 32) * 5 / 9;
